0d7dd9c632ce28acf801b12ac76772a2cff323b1
Beto Noronha <beto_noronha@live.com>
2019-09-18 10:13:25 -0300

    fix build

---
+++ build.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+##### DEPLOY
+
+echo
+echo "BUILD STARTED"
+echo
+
+echo
+echo "NEW TAG - API STAC_COMPOSE:"
+read API_STAC_COMPOSE_TAG
+
+IMAGE_API_STAC_COMPOSE="registry.dpi.inpe.br/brazildatacube/stac_compose"
+
+IMAGE_API_STAC_COMPOSE_FULL="${IMAGE_API_STAC_COMPOSE}:${API_STAC_COMPOSE_TAG}"
+
+docker build -t ${IMAGE_API_STAC_COMPOSE_FULL} -f docker/Dockerfile .
+
+docker push ${IMAGE_API_STAC_COMPOSE_FULL}
